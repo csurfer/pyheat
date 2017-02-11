5fdbf55424adb2f59aee40273287a6e388b51c84
Vishwas B Sharma <vishwassharmahello@gmail.com>
2017-02-11 09:16:04 -0800

    Pull class to package level

---
+++ pyheat/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+pyheat module
+~~~~~~~~~~~~~
+
+Usage of PyHeat class:
+
+    >>> from pyheat import PyHeat
+    >>> ph = PyHeat(<file_path>)
+    >>> ph.create_heatmap()
+    >>> ph.show_hearmap()
+
+:copyright: (c) 2017 by Vishwas B Sharma.
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'pyheat'
+__version__ = '0.0.1'
+__author__ = 'Vishwas B Sharma'
+__author_email__ = 'sharma.vishwas88@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Vishwas B Sharma'
+
+from .pyheat import PyHeat
