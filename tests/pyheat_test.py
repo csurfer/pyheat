1d06fac89039288df2d90771dee2cd1c4c91e735
Vishwas B Sharma <vishwassharmahello@gmail.com>
2017-02-11 09:17:03 -0800

    Add basic test suite.

---
+++ tests/pyheat_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+pyheat tests
+~~~~~~~~~~
+
+    Usage from git root:
+
+    >>> python setup.py test
+"""
+
+import os
+import unittest
+
+from pyheat import PyHeat
+
+
+class PyHeatTest(unittest.TestCase):
+
+    def setUp(self):
+        data_path = os.path.dirname(os.path.realpath(__file__))
+        self.pyheat = PyHeat(data_path + '/test_program.py')
+
+    def test_pyheat_show(self):
+        """Basic test to check pyheat class works end to end."""
+        self.pyheat.create_heatmap()
+        self.pyheat.show_heatmap(blocking=False)
+
+    def tearDown(self):
+        self.pyheat.close_heatmap()
+
+
+if __name__ == '__main__':
+    unittest.main()
